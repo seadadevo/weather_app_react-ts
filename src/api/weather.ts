3b46dac32169f5df2fd739e1e23aab6c1d01bb34
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-10-01 23:19:28 +0300

    making an Api configuraition files
    handle with the endpoint by using class and encapsulation

---
+++ src/api/weather.ts
@@ -0,0 +1,47 @@
+import { API_CONFIG } from "./config"
+import { Coordinates, ForecastData, GeocodingResponse, WeatherCondition, WeatherData } from "./types"
+
+class WeatherAPI {
+    private createUrl(endpoint: string, params: Record<string, number | number>) {
+        const searchParams = new URLSearchParams({
+            appid: API_CONFIG.API_KEY,
+            ...params
+        })
+        return `${endpoint}?${searchParams.toString()}`
+    }
+    private async fetchData<T>(url:string): Promise<T> {
+        const res =await fetch(url)
+        if(!res.ok) {
+            throw new Error (`Weather API Error: ${res.statusText}`)
+        }
+        return res.json()
+    }
+    async getCurrentWeather({lat, lon}: Coordinates): Promise<WeatherCondition> {
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+            lat: lat.toString(),
+            lon: lon.toString(),
+            units: API_CONFIG.DEFAULT_PARAMS.units
+        })
+
+        return this.fetchData<WeatherData>(url)
+    }
+    async getForecast({lat, lon}: Coordinates): Promise<ForecastData> {
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
+            lat: lat.toString(),
+            lon: lon.toString(),
+            units: API_CONFIG.DEFAULT_PARAMS.units
+        })
+
+        return this.fetchData<ForecastData>(url)
+    }
+    async reverseGeocode({lat, lon}: Coordinates): Promise<GeocodingResponse[]> {
+        const url = this.createUrl(`${API_CONFIG.GEO}/reverse`, {
+            lat: lat.toString(),
+            lon: lon.toString(),
+            limit: 1
+        })
+
+        return this.fetchData<GeocodingResponse[]>(url)
+    }
+    
+}
